0a6c3223c2c48ee65a045a23b81b1a7d01fe12d8
Javier Lete <javier@eskura21.com>
2022-02-21 16:39:42 +0100

    Sintaxis básica

--- MF0491/UF1842/js/bases.js
+++ MF0491/UF1842/js/bases.js
@@ -106,7 +106,7 @@ function notacionJsonArray() {
     console.log(tablero);
 }
 
-colecciones();
+// colecciones();
 
 function colecciones() {
     const col = [];
@@ -142,7 +142,7 @@ function diccionario() {
     console.log(dic.perro);
 }
 
-objetos();
+// objetos();
 
 function objetos() {
     const persona = Object();
@@ -162,4 +162,163 @@ function objetos() {
     otra.hola = 'Vale';
 
     console.log(otra);
+}
+
+// prototipos();
+
+function prototipos() {
+    function Persona(nombre, apellidos) {
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+    }
+
+    Persona.prototype.nombreCompleto = function () {
+        return this.nombre + " " + this.apellidos;
+    };
+
+    const javier = new Persona('Javier', 'Lete');
+
+    console.log(javier);
+
+    console.log(javier.nombreCompleto());
+}
+
+// clases();
+
+function clases() {
+    class Persona {
+        constructor(nombre, apellidos) {
+            this.nombre = nombre;
+            this.apellidos = apellidos;
+        }
+        nombreCompleto() {
+            return this.nombre + " " + this.apellidos;
+        }
+    }
+
+    const javier = new Persona('Javier', 'Lete');
+
+    console.log(javier);
+
+    console.log(javier.nombreCompleto());
+}
+
+// operadores();
+
+function operadores() {
+    // +, -, *, /
+
+    console.log(5 % 3); // Resto de la división entera
+
+    console.log(5 + 3);
+
+    console.log('5' + '3');
+
+    console.log(5 + '3');
+
+    console.log('5' + 3);
+
+    // >, <, >=, <=
+
+    console.log(5 == 5);
+    console.log(5 != 5);
+
+    console.log(5 == '5');
+    console.log(5 != '5');
+
+    // Estrictamente igual y distinto
+
+    console.log(5 === '5');
+    console.log(5 !== '5');
+
+    const x = 5;
+
+    console.log(1 + x + 10);
+
+    console.log(1 <= x && x <= 10);
+
+    const dato = prompt('Dime algo');
+
+    console.log(dato || 'No me has dicho nada');
+
+    console.log(!!0, !!1, !!'', !!'asdfasd', !!null, !!undefined, !!NaN);
+
+    // ? :
+
+    const cierto = confirm('¿Es cierto?');
+
+    console.log(cierto ? 'Es cierto' : 'NO es cierto');
+
+    const a = 5, b = 7;
+
+    console.log(a > b ? a : b);
+}
+
+// alternativas();
+
+function alternativas() {
+    const confirmado = confirm('¿Confirmas?');
+
+    if (confirmado) {
+        console.log('CONFIRMADO');
+    } else {
+        console.log('NO confirmado');
+    }
+
+    const mes = +prompt('Dime un mes');
+    let dias;
+
+    switch (mes) {
+        case 2:
+            dias = 28;
+            break;
+        case 4:
+        case 6:
+        case 9:
+        case 11:
+            dias = 30;
+            break;
+        default:
+            dias = 31;
+    }
+
+    console.log(mes, dias);
+}
+
+//repetitivas();
+
+function repetitivas() {
+    let c = 1;
+    while (c <= 10) {
+        console.log(c);
+        ++c;
+    }
+
+    do {
+        console.log(--c);
+    } while (c > 1);
+
+    for(let i = 1; i <= 5; i++) {
+        console.log(i);
+    }
+}
+
+repetitivasColecciones();
+
+function repetitivasColecciones() {
+    const arr = [ 1, 5, 8, 4, 3, 7, 9 ];
+    arr.hola = 'hello';
+    arr[20] = 'Otro valor';
+
+    for(let i = 0; i < arr.length; i++) {
+        console.log(i, arr[i]);
+    }
+
+    for(let clave in arr) {
+        console.log(clave, arr[clave]);
+    }
+
+    for(let dato of arr) {
+        console.log(dato);
+    }
 }
