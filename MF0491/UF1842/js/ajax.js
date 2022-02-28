8c560530103af541360ba09e8409fb9715982afe
Javier Lete <javier@eskura21.com>
2022-02-28 16:32:33 +0100

    Modal ¿estás seguro? con Bootstrap

--- MF0491/UF1842/js/ajax.js
+++ MF0491/UF1842/js/ajax.js
@@ -1,9 +1,11 @@
 'use strict';
 
 const url = 'http://localhost:3000/usuarios/';
-let listado, id, email, password, formulario;
+let listado, id, email, password, formulario, estasSeguro;
 
 document.addEventListener('DOMContentLoaded', async function () {
+    estasSeguro = document.getElementById('estasSeguro');
+
     listado = document.querySelector('#listado tbody');
 
     formulario = document.getElementById('formulario');
@@ -14,24 +16,29 @@ document.addEventListener('DOMContentLoaded', async function () {
 
     formulario.addEventListener('submit', aceptar);
 
+    activarModal();
+
     listar();
 });
 
 async function aceptar(e) {
     e.preventDefault();
 
-    const method = id.value ? 'PUT' : 'POST';
+    let metodo;
 
     const usuario = { email: email.value, password: password.value };
 
     if (id.value) {
         usuario.id = id.value;
+        metodo = 'PUT';
+    } else {
+        metodo = 'POST';
     }
 
     console.log(usuario);
 
     const respuesta = await fetch(url + id.value, {
-        method: method,
+        method: metodo,
         body: JSON.stringify(usuario),
         headers: {
             'Content-Type': 'application/json'
@@ -64,7 +71,7 @@ async function listar() {
             <td>${usuario.email}</td>
             <td> 
                 <a class="btn btn-primary" href="javascript:editar(${usuario.id})">Editar</a>
-                <a class="btn btn-danger" href="javascript:borrar(${usuario.id})">Borrar</a>
+                <a class="btn btn-danger" data-id="${usuario.id}" data-bs-toggle="modal" data-bs-target="#estasSeguro" href="javascript:borrar(${usuario.id})">Borrar</a>
             </td>`;
         listado.appendChild(tr);
     });
@@ -84,9 +91,33 @@ async function editar(idSeleccionado) {
 async function borrar(id) {
     console.log(id);
 
+    // if(!confirm(`¿Estás seguro de que quieres borrar el registro id=${id}?`)) {
+    //     return;
+    // }
+
     const respuesta = await fetch(url + id, { method: 'DELETE' });
 
     console.log(respuesta);
 
+    const modal = bootstrap.Modal.getInstance(estasSeguro);
+
+    modal.hide();
+
     listar();
+}
+function activarModal() {
+    estasSeguro.addEventListener('show.bs.modal', function (event) {
+        console.log(event);
+        // Button that triggered the modal
+        const boton = event.relatedTarget
+        // Extract info from data-bs-* attributes
+        const id = boton.getAttribute('data-id')
+        // Update the modal's content.
+        const cuerpo = estasSeguro.querySelector('.modal-body');
+
+        cuerpo.innerText = `¿Estás seguro de que quieres borrar el id ${id}?`;
+
+        const aceptar = estasSeguro.querySelector('#modal-aceptar');
+        aceptar.href = boton.href;
+    });
 }
