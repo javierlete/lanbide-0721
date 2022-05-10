d7bc93d62f1da572bce5a4fe7c39b753a689123e
javierlete <javierlete@192.168.0.42>
2022-05-10 15:29:40 +0200

    Corrección de Listener para estar en su propio paquete y con anotación

--- MF0492/UF1845/src/main/java/com/ipartek/formacion/uf1845/listeners/SesionListener.java
+++ MF0492/UF1845/src/main/java/com/ipartek/formacion/uf1845/listeners/SesionListener.java
@@ -1,11 +1,13 @@
-package com.ipartek.formacion.uf1845.controladores;
+package com.ipartek.formacion.uf1845.listeners;
 
 import java.util.*;
 
 import com.ipartek.formacion.uf1845.modelos.*;
 
+import jakarta.servlet.annotation.*;
 import jakarta.servlet.http.*;
 
+@WebListener
 public class SesionListener implements HttpSessionListener {
 
 	@Override
