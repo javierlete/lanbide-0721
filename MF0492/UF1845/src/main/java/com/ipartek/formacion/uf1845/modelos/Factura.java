0d3225a334e38972e4fb4b67ad93d7e038cafdc5
javierlete <javierlete@localhost>
2022-05-17 16:03:05 +0200

    Clases con todos los asistentes

--- MF0492/UF1845/src/main/java/com/ipartek/formacion/uf1845/modelos/Factura.java
+++ MF0492/UF1845/src/main/java/com/ipartek/formacion/uf1845/modelos/Factura.java
@@ -15,6 +15,13 @@ public class Factura {
 	
 	private ArrayList<Linea> lineas = new ArrayList<>();
 	
+	public Factura(Long id, LocalDate fecha, String numero, Cliente cliente) {
+		setId(id);
+		setFecha(fecha);
+		setNumero(numero);
+		setCliente(cliente);
+	}
+	
 	public BigDecimal getPrecio() {
 		throw new RuntimeException("NO está implementado");
 	}
@@ -25,5 +32,68 @@ public class Factura {
 	
 	public BigDecimal getTotal() {
 		return getPrecio().add(getIva());
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public LocalDate getFecha() {
+		return fecha;
+	}
+
+	public void setFecha(LocalDate fecha) {
+		this.fecha = fecha;
+	}
+
+	public String getNumero() {
+		return numero;
+	}
+
+	public void setNumero(String numero) {
+		this.numero = numero;
+	}
+
+	public Cliente getCliente() {
+		return cliente;
+	}
+
+	public void setCliente(Cliente cliente) {
+		this.cliente = cliente;
+	}
+
+	public ArrayList<Linea> getLineas() {
+		return lineas;
+	}
+
+	public void setLineas(ArrayList<Linea> lineas) {
+		this.lineas = lineas;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(cliente, fecha, id, numero);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Factura other = (Factura) obj;
+		return Objects.equals(cliente, other.cliente) && Objects.equals(fecha, other.fecha)
+				&& Objects.equals(id, other.id) && Objects.equals(numero, other.numero);
+	}
+
+	@Override
+	public String toString() {
+		return "Factura [id=" + id + ", fecha=" + fecha + ", numero=" + numero + ", cliente=" + cliente + "]";
 	}
 }
--- MF0492/UF1845/src/main/java/com/ipartek/formacion/uf1845/modelos/Linea.java
+++ MF0492/UF1845/src/main/java/com/ipartek/formacion/uf1845/modelos/Linea.java
@@ -1,11 +1,17 @@
 package com.ipartek.formacion.uf1845.modelos;
 
 import java.math.*;
+import java.util.*;
 
 public class Linea {
 	private Libro libro;
 	private Integer cantidad;
 	
+	public Linea(Libro libro, Integer cantidad) {
+		setLibro(libro);
+		setCantidad(cantidad);
+	}
+
 	public BigDecimal getPrecio() {
 		return libro.getPrecio().multiply(new BigDecimal(cantidad));
 	}
@@ -17,4 +23,44 @@ public class Linea {
 	public BigDecimal getTotal() {
 		return getPrecio().add(getIva());
 	}
+
+	public Libro getLibro() {
+		return libro;
+	}
+
+	public void setLibro(Libro libro) {
+		this.libro = libro;
+	}
+
+	public Integer getCantidad() {
+		return cantidad;
+	}
+
+	public void setCantidad(Integer cantidad) {
+		this.cantidad = cantidad;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(cantidad, libro);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Linea other = (Linea) obj;
+		return Objects.equals(cantidad, other.cantidad) && Objects.equals(libro, other.libro);
+	}
+
+	@Override
+	public String toString() {
+		return "Linea [libro=" + libro + ", cantidad=" + cantidad + "]";
+	}
+	
+	
 }
