094dd5ff954db17e4359ef6f97dbb25f244ad5a4
javierlete <javierlete@localhost>
2022-05-23 15:52:46 +0200

    Creamos constantes globales (DAO, VISTAS)

---
+++ MF0492/UF1845-Ejemplo/src/main/java/com/ipartek/formacion/mf0492/uf1845/ejemplo/controladores/Globales.java
@@ -0,0 +1,8 @@
+package com.ipartek.formacion.mf0492.uf1845.ejemplo.controladores;
+
+import com.ipartek.formacion.mf0492.uf1845.ejemplo.dal.*;
+
+public class Globales {
+	static final DaoProyecto DAO = DaoProyectoMemoria.getInstancia();
+	static final String VISTAS = "/WEB-INF/vistas";
+}
