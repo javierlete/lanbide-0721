0d9af3c658cbd95aa7f5bc8cc429512cef1dd414
javierlete <javierlete@localhost>
2022-05-23 15:52:57 +0200

    Controlador para visualizar proyectos

---
+++ MF0492/UF1845-Ejemplo/src/main/java/com/ipartek/formacion/mf0492/uf1845/ejemplo/controladores/ProyectosServlet.java
@@ -0,0 +1,23 @@
+package com.ipartek.formacion.mf0492.uf1845.ejemplo.controladores;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/proyectos")
+public class ProyectosServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setAttribute("proyectos", Globales.DAO.obtenerTodos());
+		request.getRequestDispatcher(Globales.VISTAS + "/proyectos.jsp").forward(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
