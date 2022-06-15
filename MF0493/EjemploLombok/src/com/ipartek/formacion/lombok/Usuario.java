a99f29fe14fadda8b0bcb63abfde6e106c818944
javierlete <javierlete@localhost>
2022-06-15 16:13:56 +0200

    Personalización de la entidad con anotaciones

--- MF0493/EjemploLombok/src/com/ipartek/formacion/lombok/Usuario.java
+++ MF0493/EjemploLombok/src/com/ipartek/formacion/lombok/Usuario.java
@@ -1,9 +1,14 @@
 package com.ipartek.formacion.lombok;
 
+import jakarta.persistence.*;
 import lombok.*;
 
+@Entity
+@Table(name="usuarios")
 @Data
 public class Usuario {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String email, password;
 	
