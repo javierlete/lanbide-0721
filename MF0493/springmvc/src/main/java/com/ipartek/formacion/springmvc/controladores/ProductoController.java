99f6083d7bc4705ad0e86f68b10c50bd5f858a48
javierlete <javierlete@localhost>
2022-06-21 17:10:12 +0200

    DAO con JDBC Template e inyección de dependencias

--- MF0493/springmvc/src/main/java/com/ipartek/formacion/springmvc/controladores/ProductoController.java
+++ MF0493/springmvc/src/main/java/com/ipartek/formacion/springmvc/controladores/ProductoController.java
@@ -2,15 +2,21 @@ package com.ipartek.formacion.springmvc.controladores;
 
 import javax.validation.*;
 
+import org.springframework.beans.factory.annotation.*;
 import org.springframework.stereotype.*;
 import org.springframework.validation.*;
 import org.springframework.web.bind.annotation.*;
 
 import com.ipartek.formacion.springmvc.*;
+import com.ipartek.formacion.springmvc.repositorios.*;
 
 @Controller
 @RequestMapping("/producto")
 public class ProductoController {
+	
+	@Autowired
+	private DaoProducto dao;
+	
 	@GetMapping
 	public String mostrarFormulario(Producto producto) {
 		return "producto";
@@ -21,6 +27,7 @@ public class ProductoController {
 		if (bindingResult.hasErrors()) {
 			return "producto";
 		} else {
+			dao.insertar(producto);
 			return "producto-mostrar";
 		}
 	}
---
+++ MF0493/springmvc/src/main/java/com/ipartek/formacion/springmvc/repositorios/DaoProducto.java
@@ -0,0 +1,7 @@
+package com.ipartek.formacion.springmvc.repositorios;
+
+import com.ipartek.formacion.springmvc.*;
+
+public interface DaoProducto extends Dao<Producto> {
+
+}
