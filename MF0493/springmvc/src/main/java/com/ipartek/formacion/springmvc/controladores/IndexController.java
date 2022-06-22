de03dab0698045e7d037520d8d39cc6f57333fcd
javierlete <javierlete@localhost>
2022-06-22 16:49:06 +0200

    Seguridad con formulario estándar y usuario en memoria (obsoleto)

---
+++ MF0493/springmvc/src/main/java/com/ipartek/formacion/springmvc/controladores/IndexController.java
@@ -0,0 +1,12 @@
+package com.ipartek.formacion.springmvc.controladores;
+
+import org.springframework.stereotype.*;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+public class IndexController {
+	@GetMapping("/")
+	public String index() {
+		return "index";
+	}
+}
