b2da2c88f8dcc7bae906d760171b372a7c32e2c0
javierlete <javierlete@localhost>
2022-06-27 16:45:34 +0200

    Repositorio REST resource con Spring Data REST y JPA

--- MF0493/springmvc/src/main/java/com/ipartek/formacion/springmvc/repositorios/ProductoRepository.java
+++ MF0493/springmvc/src/main/java/com/ipartek/formacion/springmvc/repositorios/ProductoRepository.java
@@ -4,9 +4,11 @@ import java.math.*;
 import java.util.*;
 
 import org.springframework.data.repository.*;
+import org.springframework.data.rest.core.annotation.*;
 
 import com.ipartek.formacion.springmvc.entidades.*;
 
-public interface ProductoRepository extends CrudRepository<Producto, Long> {
+@RepositoryRestResource(collectionResourceRel = "productos", path = "productos")
+public interface ProductoRepository extends PagingAndSortingRepository<Producto, Long> {
 	List<Producto> findByPrecioBetween(BigDecimal menor, BigDecimal mayor);
 }
