dba59801805a0ce4a4083488b8cce091cc50fc5b
javierlete <javierlete@localhost>
2022-07-05 17:19:32 +0200

    Añadimos nueva operaciones de consulta

--- MF0493/peliculas/src/main/java/com/ipartek/formacion/peliculas/repositorios/UsuarioRepository.java
+++ MF0493/peliculas/src/main/java/com/ipartek/formacion/peliculas/repositorios/UsuarioRepository.java
@@ -1,5 +1,6 @@
 package com.ipartek.formacion.peliculas.repositorios;
 
+import org.springframework.data.jpa.repository.*;
 import org.springframework.data.repository.*;
 import org.springframework.data.rest.core.annotation.*;
 
@@ -7,5 +8,10 @@ import com.ipartek.formacion.peliculas.entidades.*;
 
 @RepositoryRestResource(collectionResourceRel = "usuarios", path = "usuarios")
 public interface UsuarioRepository extends PagingAndSortingRepository<Usuario, Long> {
-
+	Usuario findByEmail(String email);
+	Usuario findByEmailAndPassword(String email, String password);
+	Iterable<Usuario> findByRol(String rol);
+	
+	@Query("select u from Usuario u where email = ?1 and rol = 'ADMIN'")
+	Usuario buscarAdminPorEmail(String email);
 }
